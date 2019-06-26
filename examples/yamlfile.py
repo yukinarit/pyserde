c8dc4dd20ad985c6d422d29128dbeb910a5e04a2
Yukinari Tani <yukinarit84@gmail.com>
2019-06-26 22:26:38 +0900

    Add yaml example

---
+++ examples/yamlfile.py
@@ -0,0 +1,83 @@
+"""
+yamlfile.py
+
+Read swagger echo example yaml.
+
+Usage:
+    $ pipenv install
+    $ pipenv run yamlfile.py
+"""
+from typing import List, Dict, Optional
+from dataclasses import dataclass
+from serde import serialize, deserialize
+from serde.yaml import from_yaml
+
+
+@deserialize(rename_all='camelcase')
+@dataclass
+class Info:
+    title: str
+    description: str
+    version: str
+
+
+@deserialize(rename_all='camelcase')
+@dataclass
+class Parameter:
+    name: str
+    # in is reserved keyword.
+    # in: str
+    type: str
+    required: bool
+
+
+@deserialize(rename_all='camelcase')
+@dataclass
+class Response:
+    description: str
+
+
+@deserialize(rename_all='camelcase')
+@dataclass
+class Path:
+    description: str
+    operation_id: str
+    parameters: List[Parameter]
+    responses: Dict[str, Response]
+
+
+@deserialize(rename_all='camelcase')
+@dataclass
+class Prop:
+    type: str
+    format: Optional[str]
+
+
+@deserialize(rename_all='camelcase')
+@dataclass
+class Definition:
+    required: Optional[List[str]]
+    properties: Dict[str, Prop]
+
+@deserialize(rename_all='camelcase')
+@dataclass
+class Swagger:
+    swagger: int
+    info: Info
+    host: str
+    schemes: List[str]
+    base_path = str
+    produces: List[str]
+    paths: Dict[str, Dict[str, Path]]
+    definitions: Dict[str, Definition]
+
+
+def main():
+    with open('swagger.yml') as f:
+        yaml = f.read()
+    swagger = from_yaml(Swagger, yaml)
+    print(swagger)
+
+
+if __name__ == '__main__':
+    main()
