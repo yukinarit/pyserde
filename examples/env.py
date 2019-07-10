59dd6f20dff31724c733ff5bae771c1224fbc2ab
Yukinari Tani <yukinarit84@gmail.com>
2019-07-10 21:09:22 +0900

    Add example of envclass integration

---
+++ examples/env.py
@@ -0,0 +1,28 @@
+from typing import List, Dict, Optional, Union
+from dataclasses import dataclass
+from envclasses import envclass, load_env
+from serde import deserialize
+from serde.yaml import from_yaml
+
+@deserialize
+@envclass
+@dataclass
+class App:
+    addr: str
+    port: int
+    secret: str
+    workers: int
+
+
+def main():
+    with open('app.yml') as f:
+        yml = f.read()
+    cfg = from_yaml(App, yml)
+    print(cfg)
+
+    load_env(cfg, prefix='APP')
+    print(cfg)
+
+
+if __name__ == '__main__':
+    main()
