e0ab7c535099f2fe50b576c5c41508bb109f2a45
Yukinari Tani <yukinarit84@gmail.com>
2019-10-16 11:00:11 +0900

    Support python 3.8

--- setup.py
+++ setup.py
@@ -63,5 +63,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
