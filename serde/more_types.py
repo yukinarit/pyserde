b06432a83bc0c734f41d0ba8eb2aac5551397fcd
Yukinari Tani <yukinarit84@gmail.com>
2019-11-25 15:08:10 +0900

    Add comment to custom (de)serializers

--- serde/more_types.py
+++ serde/more_types.py
@@ -6,6 +6,12 @@ from .core import SerdeError
 
 
 def serialize(data: Any) -> Any:
+    """
+    Custom serializer for extended types such as Decimal.
+
+    `data` shall be other than List, Dict, Tuple, Optional
+    Union, dataclass or primitives.
+    """
     if isinstance(data, Decimal):
         return str(data)
     else:
@@ -13,6 +19,12 @@ def serialize(data: Any) -> Any:
 
 
 def deserialize(field: Field, data: Any) -> Any:
+    """
+    Custom deserializer for extended types such as Decimal.
+
+    `data` shall be other than List, Dict, Tuple, Optional
+    Union, dataclass or primitives.
+    """
     if issubclass(field.type, Decimal):
         return Decimal(data)
     else:
