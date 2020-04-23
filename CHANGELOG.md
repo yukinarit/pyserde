8dcfb2814419972e2ee6a798d91fe9829467d0ab
Yukinari Tani <yukinarit84@gmail.com>
2020-04-23 20:58:23 +0900

    chore: Bump version to 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.1.1 (2020-04-23)
+
+* fix: astuple incorrectly deserialize dict ([dfd69e8](https://github.com/yukinarit/pyserde/commit/dfd69e8))
--- serde/__init__.py
+++ serde/__init__.py
@@ -6,7 +6,7 @@ from .de import deserialize, from_dict, from_obj, from_tuple, is_deserializable 
 from .se import asdict, astuple, is_serializable, serialize  # noqa
 
 """ Version of pyserde. """
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = [
     'serialize',
