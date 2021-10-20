38a9124b6f4bc8b20f18a8de3c555cb87f7e1387
Yukinari Tani <yukinarit84@gmail.com>
2021-10-20 22:08:30 +0900

    test: Add decimal and datetime examples

---
+++ examples/type_decimal.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from decimal import Decimal
+
+from serde import deserialize, serialize
+from serde.json import from_json, to_json
+
+
+@deserialize
+@serialize
+@dataclass
+class Foo:
+    v: Decimal
+
+
+def main():
+    foo = Foo(Decimal(0.1))
+    print(f"Into Json: {to_json(foo)}")
+
+    s = '{"v": "0.1000000000000000055511151231257827021181583404541015625"}'
+    print(f"From Json: {from_json(Foo, s)}")
+
+
+if __name__ == '__main__':
+    main()
