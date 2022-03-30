4fa045c5b92ee80187ff68daa3093bce42e15311
Kevin Squire <kevin@secondspectrum.com>
2022-03-30 00:13:11 -0700

    test: Add more numpy tests

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -1,5 +1,6 @@
 import logging
 from dataclasses import fields
+from typing import List
 
 import numpy as np
 import numpy.typing as npt
@@ -8,7 +9,7 @@ import pytest
 import serde
 from serde.compat import get_origin
 
-from .common import all_formats, opt_case, opt_case_ids
+from .common import all_formats, format_json, format_msgpack, opt_case, opt_case_ids
 
 log = logging.getLogger("test")
 
@@ -80,3 +81,109 @@ def test_simple(se, de, opt):
 
         assert type(de_value) == typ
         assert type(de_value).dtype == field.type.dtype
+
+
+@pytest.mark.parametrize("opt", opt_case, ids=opt_case_ids())
+@pytest.mark.parametrize("se,de", format_json + format_msgpack)
+def test_encode_numpy(se, de, opt):
+    log.info(f"Running test with se={se.__name__} de={de.__name__} opts={opt}")
+
+    de_int = de(int, se(np.int32(1)))
+    assert de_int == 1
+    assert isinstance(de_int, int)
+
+    de_arr = de(list, se(np.array([1, 2, 3])))
+    assert isinstance(de_arr, list)
+    assert de_arr == [1, 2, 3]
+
+    @serde.serde(**opt)
+    class MisTyped:
+        a: int
+        b: float
+        h: bool
+        c: List[int]
+        d: List[float]
+        e: List[bool]
+
+    expected = MisTyped(1, 3.0, False, [1, 2], [5.0, 6.0], [True, False])
+
+    test1 = MisTyped(
+        np.int32(1),
+        np.float32(3.0),
+        np.bool_(False),
+        np.array([np.int32(i) for i in [1, 2]]),
+        np.array([np.float32(i) for i in [5.0, 6.0]]),
+        np.array([np.bool_(i) for i in [True, False]]),
+    )
+
+    assert de(MisTyped, se(test1)) == expected
+
+    test2 = MisTyped(
+        np.int64(1),
+        np.float64(3.0),
+        np.bool_(False),
+        np.array([np.int64(i) for i in [1, 2]]),
+        np.array([np.float64(i) for i in [5.0, 6.0]]),
+        np.array([np.bool_(i) for i in [True, False]]),
+    )
+
+    assert de(MisTyped, se(test2)) == expected
+
+    test3 = MisTyped(
+        np.int64(1),
+        np.float64(3.0),
+        np.bool_(False),
+        np.array([np.int64(i) for i in [1, 2]]),
+        np.array([np.float64(i) for i in [5.0, 6.0]]),
+        np.array([np.bool_(i) for i in [True, False]]),
+    )
+
+    assert de(MisTyped, se(test2)) == expected
+
+
+@pytest.mark.parametrize("se,de", format_json + format_msgpack)
+def test_encode_numpy_with_no_default_encoder(se, de):
+    log.info(f"Running test with se={se.__name__} de={de.__name__} with no default encoder")
+
+    with pytest.raises(TypeError):
+        se(np.int32(1), default=None)
+
+    with pytest.raises(TypeError):
+        se(np.array([1, 2, 3]), default=None)
+
+    @serde.serde
+    class MisTypedNoDefaultEncoder:
+        a: int
+        b: float
+        h: bool
+        c: List[int]
+        d: List[float]
+        e: List[bool]
+
+    test1 = MisTypedNoDefaultEncoder(
+        np.int32(1),
+        np.float32(3.0),
+        np.bool_(False),
+        np.array([np.int32(i) for i in [1, 2]]),
+        np.array([np.float32(i) for i in [5.0, 6.0]]),
+        np.array([np.bool_(i) for i in [True, False]]),
+    )
+
+    with pytest.raises(TypeError):
+        se(test1, default=None)
+
+    test2 = MisTypedNoDefaultEncoder(
+        np.int64(1),
+        np.float64(3.0),
+        np.bool_(False),
+        np.array([np.int64(i) for i in [1, 2]]),
+        np.array([np.float64(i) for i in [5.0, 6.0]]),
+        np.array([np.bool_(i) for i in [True, False]]),
+    )
+
+    with pytest.raises(TypeError):
+        se(test2, default=None)
+
+
+def test_numpy_misc():
+    assert serde.numpy.fullname(str) == "str"
