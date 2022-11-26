1a0f7e9ce1be0e5d6003375b5eed517eaf670165
Doering Christian <christian.doering@tum.de>
2022-11-26 19:39:00 +0100

    Formatting fix

--- serde/pickle.py
+++ serde/pickle.py
@@ -1,9 +1,8 @@
 """
 Serialize and Deserialize in Pickle format.
 """
-from typing import Type
-
 import pickle
+from typing import Type
 
 from .compat import T
 from .de import Deserializer, from_dict
@@ -11,15 +10,18 @@ from .se import Serializer, to_dict
 
 __all__ = ["from_pickle", "to_pickle"]
 
+
 class PickleSerializer(Serializer):
     @classmethod
     def serialize(cls, obj, **opts) -> bytes:
         return pickle.dumps(obj, **opts)
 
+
 class PickleDeserializer(Deserializer):
     @classmethod
     def deserialize(cls, data, **opts):
         return pickle.loads(data, **opts)
+
 
 def to_pickle(obj, se: Type[Serializer] = PickleSerializer, **opts) -> str:
     return se.serialize(to_dict(obj, reuse_instances=False), **opts)
@@ -27,4 +29,3 @@ def to_pickle(obj, se: Type[Serializer] = PickleSerializer, **opts) -> str:
 
 def from_pickle(c: Type[T], s: str, de: Type[Deserializer] = PickleDeserializer, **opts) -> T:
     return from_dict(c, de.deserialize(s, **opts), reuse_instances=False)
-    
