ed5c98e8bfaa5e7a547e358e0eb2bfb47d37a806
yukinarit <yukinarit84@gmail.com>
2024-01-06 12:59:07 +0900

    Fix type error from field with attributes
    This commit fixes pyright type error from field with attributes.
    You still get type error with mypy v1.8.0 as mypy still does not
    fully support dataclass_transform
    ```
    from dataclasses import dataclass
    from serde import serde, field
    
    @serde
    @dataclass
    class Foo:
        id: int = field(rename="ID")
        comments: str
    ```

---
+++ examples/field_order.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from serde import serde, field
+
+
+@serde
+@dataclass
+class Foo:
+    id: int = field(rename="ID")  # Field with attributes can be defined before other fields
+    # thanks to dataclass_transform field_specifiers
+    # https://peps.python.org/pep-0681/#the-dataclass-transform-decorator
+    # NOTE: you still get error with mypy as of mypy v1.8.0
+    comments: str
+
+
+if __name__ == "__main__":
+    pass
